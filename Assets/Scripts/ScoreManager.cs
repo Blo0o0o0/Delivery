27210ab43ee0206481079c495f8ee1d9ee75c4b4
Blo0o0o0 <zachariah.lee2003@gmail.com>
2023-04-29 18:33:12 +0100

    added score script

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+
+    int score = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Points: " + score.ToString();
+    }
+
+    void Update()
+    {
+        scoreText.text = "Points: " + score.ToString();
+    }
+
+    public void AddPoints(int points)
+    {
+        score += points;
+        scoreText.text = "Points: " + score.ToString();
+    }
+
+    public int GetPoints()
+    {
+        return score;
+    }
+
+    public void setPoints(int amount)
+    {
+        score = amount;
+    }
+}
