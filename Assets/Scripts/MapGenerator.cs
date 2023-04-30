dfd6b4cc958c5b3bcb454215c075c1ead03715d0
Thomas <60945086+thomas-chernaik@users.noreply.github.com>
2023-05-01 00:53:45 +0100

    added all sprites for shops

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,13 +7,22 @@ public class MapGenerator : MonoBehaviour
     public float tileSize;
     public float tileScale;
     public Vector3 startPosition;
-    public GameObject end;
-    public GameObject tJunction;
-    public GameObject straight;
-    public GameObject turn;
-    public GameObject grass;
-    public GameObject cross;
+    public GameObject end;//e
+    public GameObject tJunction;//t
+    public GameObject straight;//s
+    public GameObject turn;//c
+    public GameObject grass;//g
+    public GameObject cross;//x
+    public GameObject butchers;//b
+    public GameObject bakers;//a
+    public GameObject dairy;//d
+    public GameObject takeaway;//k
+    public GameObject iceCream;//i
+    public GameObject pizzaria;//p
+    public GameObject curry;//u
+    public GameObject groceries;//r
     public TextAsset map;
+    
 
     string fileContents;
     // Start is called before the first frame update
@@ -54,7 +63,41 @@ public class MapGenerator : MonoBehaviour
                     case 'g':
                         obj = Instantiate(grass);
                         break;
+                    case 'b':
+                        obj = Instantiate(butchers);
+                        obj.GetComponent<GeneratorBuilding>().manager = GetComponent<GeneratorManager>();
+                        break;
+                    case 'a':
+                        obj = Instantiate(bakers);
+                        obj.GetComponent<GeneratorBuilding>().manager = GetComponent<GeneratorManager>();
+                        break;
+                    case 'd':
+                        obj = Instantiate(dairy);
+                        obj.GetComponent<GeneratorBuilding>().manager = GetComponent<GeneratorManager>();
+                        break;
+                    case 'i':
+                        obj = Instantiate(iceCream);
+                        obj.GetComponent<GeneratorBuilding>().manager = GetComponent<GeneratorManager>();
+                        break;
+                    case 'k':
+                        obj = Instantiate(takeaway);
+                        obj.GetComponent<GeneratorBuilding>().manager = GetComponent<GeneratorManager>();
+                        break;
+                    case 'p':
+                        obj = Instantiate(pizzaria);
+                        obj.GetComponent<GeneratorBuilding>().manager = GetComponent<GeneratorManager>();
+                        break;
+                    case 'u':
+                        obj = Instantiate(curry);
+                        obj.GetComponent<GeneratorBuilding>().manager = GetComponent<GeneratorManager>();
+                        break;
+                    case 'r':
+                        obj = Instantiate(groceries);
+                        obj.GetComponent<GeneratorBuilding>().manager = GetComponent<GeneratorManager>();
+                        break;
+
                 }
+                //badkipur
                 obj.transform.position = startPosition + tileSize * tileScale * x * Vector3.right + tileSize * tileScale * y * Vector3.forward;
                 obj.transform.rotation = Quaternion.Euler(new Vector3(obj.transform.rotation.eulerAngles.x, 90 * numRotations, obj.transform.eulerAngles.z));
                 //obj.transform.Rotate(0, 90 * numRotations, 0);
