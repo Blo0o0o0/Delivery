ecf59bd124b50c41cae1c41b8c2e3cbac70c7fbb
Thomas <60945086+thomas-chernaik@users.noreply.github.com>
2023-05-01 15:18:53 +0100

    created some camera animations for sucking and throwing

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,11 +6,14 @@ public class CameraFollow : MonoBehaviour
 {
     public float smoothness;
     public Transform targetObject;
+    public Animator otherAnim;
     private Vector3 initalOffset;
     private Vector3 cameraPosition;
+    private Animator anim;
 
     void Start()
-    {  
+    {
+        anim = GetComponent<Animator>();
         initalOffset = transform.position - targetObject.position;
     }
 
@@ -18,5 +21,14 @@ public class CameraFollow : MonoBehaviour
     {
         cameraPosition = targetObject.position + initalOffset;
         transform.position = Vector3.Lerp(transform.position, cameraPosition, smoothness*Time.deltaTime);
+        if(Input.GetMouseButton(1))
+        {
+            anim.SetBool("sucking", true);
+        }
+        else
+        {
+            anim.SetBool("sucking", false);
+        }
+        anim.SetBool("throwing", otherAnim.GetBool("Launching"));
     }
 }
